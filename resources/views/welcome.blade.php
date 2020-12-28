2e06e5026171592bedb307d6f4f82dfbfc21818d
bhhussain <bhhussain@gmail.com>
2020-12-28 14:03:53 +0400

    Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <head>
     <meta charset="utf-8">
