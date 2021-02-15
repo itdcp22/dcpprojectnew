92e2b540ab384928badb19350f44b766e5916310
bhhussain <bhhussain@gmail.com>
2021-02-15 16:53:57 +0400

    Commit

--- app/Http/Controllers/Admin/AdvancehistoryController.php
+++ app/Http/Controllers/Admin/AdvancehistoryController.php
@@ -32,7 +32,8 @@ class AdvancehistoryController extends Controller
         //return view('admin.advancehistory.advpending')->with($arr);
 
         $arr['advances'] = Advance::where('ca_comp_code', auth()->user()->company)
-            ->where('ca_status', 1)
+            ->where('ca_status', 0)
+            ->where('ca_pay_status', 1)
             ->groupBy('ca_emp_name')
             ->selectRaw('ca_emp_name,sum(ca_adv_amt) as total')
             ->get();
