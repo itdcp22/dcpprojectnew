961229d43c5efbf81d0bf87239d3cf48b9c21bfa
bhhussain <bhhussain@gmail.com>
2021-07-11 11:37:51 +0400

    Commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -549,7 +549,7 @@
 
                 <li class="nav-item has-treeview">
                   <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-shopping-cart"></i>
+                    <i class="nav-icon fas fa-cog"></i>
 
                     <p>
                       Others
