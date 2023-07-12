150256b72549e3af92e4ca979a3e9e0b326a9f41
itdcp22 <itdcp22@gmail.com>
2023-07-12 22:18:43 +0400

    commit

--- resources/views/calander/index.blade.php
+++ resources/views/calander/index.blade.php
@@ -32,7 +32,7 @@
             events : [
                 @foreach($booking as $booking)
                 {
-                    title : '{{ $booking->tb_reference }} - {{ $booking->tb_cust_name }}',
+                    title : '{{ $booking->tb_cust_name }} - {{ $booking->tb_time }}',
                     start : '{{ $booking->tb_date }}',
                     url : '{{ route('foh.booking.show', $booking->id) }}'
                     
